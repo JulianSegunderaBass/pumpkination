20735cce6bd22506643e544cc6c3b02a6da16d3f
JulianSegunderaBass <32948845+JulianSegunderaBass@users.noreply.github.com>
2019-11-15 22:13:38 +0800

    Update and rename PumpkinGallery.html to PumpkinGallery.php

--- PumpkinGallery.php
+++ PumpkinGallery.php
@@ -94,10 +94,10 @@
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="PumpkinInfo.html"><i class="fas fa-book"></i> Read about Pumpkins<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="PumpkinInfo.php"><i class="fas fa-book"></i> Read about Pumpkins<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link" href="PumpkinGallery.html"><i class="fas fa-image"></i> The Image Gallery<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="PumpkinGallery.php"><i class="fas fa-image"></i> The Image Gallery<span class="sr-only">(current)</span></a>
                     </li>
                 </ul>
             </div>
