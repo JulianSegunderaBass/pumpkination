432ccc14e4cbc26eaad34d6e7bbb31eb2036b552
JulianSegunderaBass <32948845+JulianSegunderaBass@users.noreply.github.com>
2019-11-15 22:19:54 +0800

    Update PumpkinGallery.php

--- PumpkinGallery.php
+++ PumpkinGallery.php
@@ -87,7 +87,7 @@
 
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
         <div class="container">
-            <a class="navbar-brand" href="index.html"><i class="fas fa-home"></i> Pumpkination</a>
+            <a class="navbar-brand" href="index.php"><i class="fas fa-home"></i> Pumpkination</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
